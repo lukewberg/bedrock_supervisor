09736b8a24c7eab5d1fa931f1f7cedeec2c7c724
Luke Berg <lberg@commerce-architects.com>
2025-04-21 14:29:46 -0700

    More work on backups

--- bedrockd/src/backup.rs
+++ bedrockd/src/backup.rs
@@ -1,24 +1,42 @@
 use crate::management::rcon::ServerStdioResponse;
+use std::io::Write;
 use std::path::PathBuf;
-use std::process::Stdio;
+use std::process::{ChildStdin, ChildStdout, Stdio};
 use tokio::sync::mpsc::Sender;
-use tonic::Status;
+use tonic::{Code, Status};
 // pub fn backup(iostream: Stdio)
 
 pub struct BackupManager {
     pub frequency: u16,
     pub dir: PathBuf,
+    pub stdin: Option<ChildStdin>,
+    pub stdout: Option<ChildStdout>,
 }
 
 impl BackupManager {
     pub fn new(frequency: u16, dir: PathBuf) -> BackupManager {
-        Self { frequency, dir }
+        Self {
+            frequency,
+            dir,
+            stdin: None,
+            stdout: None,
+        }
     }
 
     pub async fn initiate_backup(
-        &self,
+        &mut self,
         tx: Sender<ServerStdioResponse>,
     ) -> Result<ServerStdioResponse, Status> {
+        if let Some(mut stdin) = self.stdin.take() {
+            let result = tokio::task::spawn(async move {
+                stdin
+                    .write_all("".as_bytes())
+                    .expect("Unable to write bytes to stdin!")
+            })
+            .await
+            .or_else(|e| Err(Status::new(Code::Internal, e.to_string())));
+        } else {
+        };
         todo!()
     }
 }
